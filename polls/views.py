46d56c9b03c17b9d263a3791679228548f8315c0
itsjuniordias <juniordias_@live.com>
2019-11-19 11:15:56 -0300

    create method Vote

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
+from django.urls import reverse
+
+from .models import Choice, Question
 
 def index(request):
   latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -12,8 +14,20 @@ def detail(request, question_id):
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-  response = "You're looking at the results of question %s."
-  return HttpResponse(response % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-  return HttpResponse("You're voting on question %s. " % question_id)
+  question = get_object_or_404(Question, pk=question_id)
+  try:
+    select_choice = question.choice_set.get(pk=request.POST['choice'])
+  except(KeyError, Choice.DoesNotExists):
+    return render(request, 'polls/detail.html', {
+      'question': question,
+      'error_message': "You didn't select a choice.",
+    })
+  else:
+    select_choice.votes += 1
+    select_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+
