37ed5680ba968c1cb0ab81f8ecd6136f818aef1d
itsjuniordias <juniordias_@live.com>
2019-11-19 11:20:03 -0300

    create method Results

--- polls/views.py
+++ polls/views.py
@@ -31,3 +31,7 @@ def vote(request, question_id):
     select_choice.save()
     return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
 
+def results(request, question_id):
+  question = get_object_or_404(Question, pk=question_id)
+  return render(request, 'polls/results.html', {'question':question})
+
